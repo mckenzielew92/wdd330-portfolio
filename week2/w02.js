926fe7f31308bbe64b5695ac7b0af5367cedb622
mckenzielew92 <mckenzielew92@yahoo.com>
2021-09-24 07:11:25 -0600

    notes

--- week2/w02.js
+++ week2/w02.js
@@ -1,16 +1,31 @@
-function addnumbers(){
-    //collects
-    let x = parseFloat(document.getElementById("numimput1").value);
+function addNumbers(){
+    //collects numbers from input id numInput1
+    let x = parseInt(document.getElementById("numInput1").value);
+    //defines output value
     let output = 0;
-
-    for(let i = 0; i <= x; i++){
-        output += i;
+    //creates loop for i to = to numInput1 value
+    if(isNaN(x)){
+        alert("Invalid input Please use a number");
+    } else{
+        for(let i = 0; i <= x; i++){
+            //adds i current value you output
+            output += i;
+        }
     }
+    
 
     //take let output and places it to the div output.
     document.getElementById("output1").innerHTML = output;
 }
 
-function sum({
+function addNumberTwo(){
+    //collects number from id numInput1
+    let x = parseInt(document.getElementById("numInput1").value);
+    //collects nub
+    let y = parseInt(document.getElementById("numInput2").value);
+    //adds x and y together for output
+    let output = x + y
 
-})
+    //take let output and places it to the div output.
+    document.getElementById("output2").innerHTML = output;
+}
