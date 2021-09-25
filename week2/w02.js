4f190ea0ab1c5465c3bc7e2211c379bd42998866
mckenzielew92 <mckenzielew92@yahoo.com>
2021-09-24 22:05:29 -0600

    push

--- week2/w02.js
+++ week2/w02.js
@@ -1,31 +1,31 @@
 function addNumbers(){
-    //collects numbers from input id input1
+     //collects numbers from input id input1
     let x = parseInt(document.getElementById("input1").value);
     //defines output value
     let output = 0;
     //creates loop for i to = to input1 value
-    if(isNaN(x)){
+    if (isNaN(x)) {
         alert("Invalid input Please use a number");
-    } else{
-        for(let i = 0; i <= x; i++){
+    } else {
+        for (let i = 0; i <= x; i++) {
             //adds i current value you output
             output += i;
         }
     }
     
-
     //take let output and places it to the div output.
     document.getElementById("output1").innerHTML = output;
 }
 
-function addNumber2(){
-    //collects number from id numInput1
+
+
+function addNumbers2(){
+    //collects number from id input1
     let x = parseInt(document.getElementById("input1").value);
-    //collects nub
-    let y = parseInt(document.getElementById("output2").value);
+    //collects number from id input2
+    let y = parseInt(document.getElementById("input2").value);
     //adds x and y together for output
     let output = x + y
-
     //take let output and places it to the div output.
     document.getElementById("output2").innerHTML = output;
 }
