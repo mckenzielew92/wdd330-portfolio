b7a1a60768270a52947588264e6fded63be2ee25
mckenzielew92 <mckenzielew92@yahoo.com>
2021-10-09 17:22:19 -0600

    push

--- week4/main.js
+++ week4/main.js
@@ -11,7 +11,7 @@
 
 const form = document.forms['search'];
 
-const input = form.elements['searchInput']
+const input = form.elements['searchInput'];
 //const input = form['searchInput'];
 //these will add certain actions to the form lines 14-18
 //input.addEventListener('focus', () => alert('focused'), false);
@@ -53,5 +53,47 @@ input.addEventListener('blur', function(){
 //Now the default text will disappear when the user clicks inside the input field (the focus event) and reappear if the user leaves the field blank and clicks away from it (the blur event).
 
 
+/**************************Object Oriented Programming Notes*********************************/
 
+/**Encapsulation:**/
 
+//not object oriented
+let baseSalary = 30_000;
+let overtime = 10;
+let rate = 20;
+
+function getWage(baseSalary, overtime, rate) {
+    return baseSalary + (overTime * rate);
+}
+
+// object oriented way:
+
+//employee object with 3 properties, method called getWage()
+let employee = {
+    /*there are no parameters in this way because these properties are
+    actually properties of the object. The best functions are the ones with no parameters!
+    The easier to maintain the function*/
+    baseSalary: 30_000,
+    overtime: 10,
+    rate: 20,
+    getWage: function() {
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+};
+employee.getWage();
+
+/**Polymorphism**/
+
+/*WITHOUT OBJECT ORIENTED PROGRAMMING:
+switch(...) {
+    case 'select': rederSelect();
+    case : 'text': renderTextBox();
+    case 'checkbox' : renderCheckbox();
+    case ...;
+    case ...;
+    case ...
+}  */ 
+
+//object oriented way:
+
+elements.render();
